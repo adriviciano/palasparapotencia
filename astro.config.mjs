57383f5976aed908c045f795b849ea00ecdd3756
adriviciano <adriviciano@gmail.com>
2025-04-11 08:25:30 +0200

    solve .jsx error

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
-import { defineConfig } from 'astro/config'
+import { defineConfig } from 'astro/config';
+import react from '@astrojs/react';
 
 export default defineConfig({
-  site: 'https://adriviciano.github.io',
-  base: 'palasparapotencia',
-})
+  integrations: [react()],
+  output: 'static',
+  base: '/palasparapotencia/',
+});
